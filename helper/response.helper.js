522468dacb8ac8b9fa51593a614104f03a714a4c
Afizahmad Fairuz <afizahmadfairuz@gmail.com>
2024-05-10 13:55:38 +0700

    chapter4

---
+++ helper/response.helper.js
@@ -0,0 +1,19 @@
+const response = {
+  code: null,
+  message: null,
+  data: null,
+  error: null,
+};
+
+const responsePagination = {
+  code: null,
+  message: null,
+  data: null,
+  perPage: null,
+  page: null,
+};
+
+module.exports = {
+  response,
+  responsePagination,
+};
